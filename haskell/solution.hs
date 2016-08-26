0376a7472bdf5d1429535279e2330c176edf99a4
Danilo <dznery@gmail.com>
2016-08-26 19:15:18 -0300

    simpler solution in haskell

--- haskell/solution.hs
+++ haskell/solution.hs
@@ -1,4 +1,4 @@
-import Data.List
+import Data.List.Utils -- cabal install missingH
 
 -- solution.hs
 --      reflow and justify input text to 40-char width.
@@ -6,37 +6,22 @@ import Data.List
 --      results can actually be seen. e.g.:
 --      putStrLn(prettify "sample text")
 
--- curryfying function
+-- solution entry point
 prettify :: [Char] -> [Char]
 prettify [] = []
-prettify a = concat(justify(reflow a))
+prettify a = concat(justify(reflow(words a)))
 
--- reflow entry function
-reflow :: [Char] -> [[Char]]
-reflow [] = []
-reflow a =
-        let (b, c) = reflow'(take 41 a)
-        in b:reflow(c ++ (drop 41 a))
-
--- reflow "tokenizer" function
-reflow' :: [Char] -> ([Char], [Char])
-reflow' [] = ([], [])
-reflow' a
-        | length a <= 40 = (a, [])
-        -- also swap last char of 'c' for '\n'
-        | otherwise = (init(reverse c) ++ "\n", reverse b)
-          where (b, c) = span(/= ' ')(reverse a)
+-- reflow word list function
+reflow:: [[Char]] -> [[Char]]
+reflow a@(_:[]) = a
+reflow a@(x:y:_)
+  | length(x ++ " " ++ y) <= 40 = reflow((x ++ " " ++ y):tail(tail a))
+  | otherwise = (x ++ "\n"):[] ++ reflow(tail a)
 
 -- justify line list function
 justify :: [[Char]] -> [[Char]]
-justify a@(x:[]) = a
-justify a@(x:y:_)
-        -- if justified line is SEQ to previous, then commit action
-        | length(findIndices(== ' ') y) + (length y) <= length x
-        = justify(x:concat(map justify' y):tail(tail a))
-        | otherwise = x:justify(tail a)
-
--- justify map function
-justify' :: Char -> [Char]
-justify' ' ' = "  "
-justify' c = c:[]
+justify a@(_:[]) = a
+justify a@(x:_)
+  | ((countElem ' ' x) + (length x) <= 40)
+  = justify((replace " " "  " x):(tail a))
+  | otherwise = x:[] ++ justify(tail a)
